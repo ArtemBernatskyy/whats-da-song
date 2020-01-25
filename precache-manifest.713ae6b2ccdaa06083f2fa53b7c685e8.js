71628cf768ec7a25abb3784ba4ffd90f9f5c3127
Artem Bernatskyi <artem.bernatskyy@gmail.com>
2020-01-26 00:51:23 +0200

    Updates

---
+++ precache-manifest.713ae6b2ccdaa06083f2fa53b7c685e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67be127cb39baee40eeeccfbeab63b48",
+    "url": "/whats-da-song/index.html"
+  },
+  {
+    "revision": "c567ab78e1b49299714e",
+    "url": "/whats-da-song/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "b8a0d388f0b3c7b966b4",
+    "url": "/whats-da-song/static/css/main.61c289bf.chunk.css"
+  },
+  {
+    "revision": "c567ab78e1b49299714e",
+    "url": "/whats-da-song/static/js/2.91e5c937.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/whats-da-song/static/js/2.91e5c937.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8a0d388f0b3c7b966b4",
+    "url": "/whats-da-song/static/js/main.d378ba8a.chunk.js"
+  },
+  {
+    "revision": "20e87b52b7a5b8462fce",
+    "url": "/whats-da-song/static/js/runtime-main.68bee81e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/whats-da-song/precache-manifest.8542e700b084b7c32207f0383bd836fd.js"
+  "/whats-da-song/precache-manifest.713ae6b2ccdaa06083f2fa53b7c685e8.js"
 );
 
 self.addEventListener('message', (event) => {
