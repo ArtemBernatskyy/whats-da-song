49feb3b131279b26b0207d8e7ae648bbf9600b42
Artem Bernatskyi <artem.bernatskyy@gmail.com>
2020-01-25 22:21:17 +0200

    Updates

---
+++ precache-manifest.8542e700b084b7c32207f0383bd836fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21193dfd2e3286ed721cd802840553bc",
+    "url": "/whats-da-song/index.html"
+  },
+  {
+    "revision": "ce402b7aa13293791d82",
+    "url": "/whats-da-song/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "3da04ac5bde8bd452202",
+    "url": "/whats-da-song/static/css/main.b2268e10.chunk.css"
+  },
+  {
+    "revision": "ce402b7aa13293791d82",
+    "url": "/whats-da-song/static/js/2.d371c63f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/whats-da-song/static/js/2.d371c63f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3da04ac5bde8bd452202",
+    "url": "/whats-da-song/static/js/main.a3af3051.chunk.js"
+  },
+  {
+    "revision": "20e87b52b7a5b8462fce",
+    "url": "/whats-da-song/static/js/runtime-main.68bee81e.js"
+  }
+]);
